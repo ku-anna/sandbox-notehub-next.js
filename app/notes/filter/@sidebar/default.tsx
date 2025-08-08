8e7c17424d65cd3430381cfefeb066d8dc312cfc
Ann K <kubareva.ann@gmail.com>
2025-08-08 15:26:46 +0300

    x

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,8 +1,21 @@
-// app/notes/filter/@sidebar/default.tsx
+import Link from "next/link";
+import { getCategories } from "@/lib/api";
+import css from "./sidebar.module.css";
 
 const NotesSidebar = async () => {
+  const categories = await getCategories();
+
   return (
-    <div style={{ fontSize: "24px", fontWeight: "bold" }}>Notes Sidebar</div>
+    <ul className={css.sidebar}>
+      <li className={css.sidebarItem}>
+        <Link href={`/notes/filter/all`}>All notes</Link>
+      </li>
+      {categories.map((category) => (
+        <li key={category.id} className={css.sidebarItem}>
+          <Link href={`/notes/filter/${category.id}`}>{category.name}</Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
